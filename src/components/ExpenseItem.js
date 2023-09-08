bf632fad0cc97cef760d0d48d5161cf5bfca0866
NicholasDevelop <marcaccini.n@gmail.com>
2023-09-08 17:19:26 +0200

    Start project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,27 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  // function ExpenseItem({date, title, amount}) {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
